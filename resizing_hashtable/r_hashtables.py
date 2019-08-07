726757738084d2926ffc7079021ad6c21096286b
Andrew Richardson <andrewlowellr@gmail.com>
2019-08-07 10:41:37 -0600

    Finish day 2 MVP.

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -17,14 +17,20 @@ class LinkedPair:
 # '''
 class HashTable:
     def __init__(self, capacity):
-        pass
+        self.storage = [None] * capacity
+        self.capacity = capacity
+        self.original_capacity = capacity
 
 
 # '''
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 5381
+    for char in string:
+        hash = ((hash << 5) + hash) + ord(char)
+
+    return (hash & 0xFFFFFFFF) % max
 
 
 # '''
@@ -33,7 +39,13 @@ def hash(string, max):
 # Hint: Used the LL to handle collisions
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    hashed = hash(key, hash_table.capacity)
+    if hash_table.storage[hashed] == None:
+        hash_table.storage[hashed] = LinkedPair(key, value)
+    else:
+        collided = LinkedPair(key, value)
+        collided.next = hash_table.storage[hashed]
+        hash_table.storage[hashed] = collided
 
 
 # '''
@@ -42,7 +54,19 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    hashed = hash(key, hash_table.capacity)
+
+    if hash_table.storage[hashed] == None:
+        print(f"{key} doesn't exist")
+    else:
+        current = hash_table.storage[hashed]
+        if current.key == key:
+            hash_table.storage[hashed] = current.next
+        else:
+            while(current != None):
+                if current.next.key == key:
+                    current.next = current.next.next
+                current = current.next
 
 
 # '''
@@ -51,14 +75,34 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    hashed = hash(key, hash_table.capacity)
+
+    if hash_table.storage[hashed] == None:
+        return None
+    else:
+        current = hash_table.storage[hashed]
+        if current.key == key:
+            return current.value
+        else:
+            while(current != None):
+                if current.next.key == key:
+                    return current.next.value
+                current = current.next
+
+            return None
 
 
 # '''
 # Fill this in
 # '''
 def hash_table_resize(hash_table):
-    pass
+    resized = HashTable(hash_table.capacity * 2)
+    for item in hash_table.storage:
+        current = item
+        while(current != None):
+            hash_table_insert(resized, current.key, current.value)
+            current = current.next
+    return resized
 
 
 def Testing():
